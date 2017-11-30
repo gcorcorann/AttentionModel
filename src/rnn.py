7cc1e767ccf41c4cb919bf12b1efca600b3566f4
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2017-11-30 15:59:47 -0500

    Added new RNN models (based on image capturing), and it's corresponding training function.

--- src/rnn.py
+++ src/rnn.py
@@ -48,6 +48,42 @@ class RNN(nn.Module):
         """
         return Variable(torch.zeros(batch_size, self.hidden_size))
 
+class RNN2(nn.Module):
+    """ RNN based on image captioning. """
+    def __init__(self, input_size, hidden_size, output_size):
+        """
+        Initialize RNN class.
+
+        @param  input_size:     input dimension size
+        @param  hidden_size:    number of hidden nodes
+        @param  output_size:    number of output units
+        """
+        super(RNN2, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.o2o = nn.Linear(hidden_size + output_size, output_size)
+        self.softmax = nn.LogSoftmax()
+
+    def forward(self, input, hidden):
+        """
+        Forward pass through network.
+        """
+        input_combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(input_combined)
+        output = self.i2o(input_combined)
+        output_combined = torch.cat((hidden, output), 1)
+        output = self.o2o(output_combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def init_hidden(self, batch_size):
+        """
+        Initialize hidden units to zero.
+        """
+        return Variable(torch.zeros(batch_size, self.hidden_size))
+
 
 # HELPER FUNCTIONS #
 import random
@@ -94,13 +130,14 @@ def random_training_example(X_train, y_train, batch_size):
     return x, x_var, y, y_var
 
 def train(rnn, X_train, y_train, criterion, optimizer):
-    batch_size = 10
+    batch_size = 5
     print('Training Model...')
-    for epoch in range(10):
+    for epoch in range(3):
         print('Epoch:', epoch)
         running_loss = 0.0
         # go through all training examples
         for i in range(X_train.shape[0]//batch_size):
+            print(i)
             # get batch of random training examples
             # nSequence x nBatch x nFeatures
             x, x_var, y, y_var = random_training_example(X_train, y_train, 
@@ -124,12 +161,44 @@ def train(rnn, X_train, y_train, criterion, optimizer):
 
     print('Finished Training.')
 
+def train2(rnn, X_train, y_train, criterion, optimizer):
+    batch_size = 5
+    print('Training Model...')
+    for epoch in range(5):
+        print('Epoch:', epoch)
+        running_loss = 0.0
+        # go through all training examples
+        for i in range(X_train.shape[0]//batch_size):
+            # get batch of random training examples
+            # nSequence x nBatch x nFeatures
+            x, x_var, y, y_var = random_training_example(X_train, y_train, 
+                    batch_size)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # zero initial hidden state
+            hidden = rnn.init_hidden(batch_size)
+            # for each frame in sequence
+            loss = 0
+            for j in range(x_var.size()[0]):
+                # pass through RNN
+                output, hidden = rnn(x_var[j], hidden)
+                loss += criterion(output, y_var)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.data[0]
+        
+        # print statistics
+        print('Epoch Loss:', running_loss / X_train.shape[0] * batch_size)
+        running_loss = 0
+
+    print('Finished Training.')
+
 def main():
     """ Main Function. """
     print(__doc__)
     # load small flow dataset
-    X_train, y_train = read_data('../data/X_flow_med.npy',
-            '../data/y_flow_med.npy')
+    X_train, y_train = read_data('../data/X_flow_small.npy',
+            '../data/y_flow_small.npy')
     print('X_train:', X_train.shape)
     print('y_train:', y_train.shape)
     print()
@@ -137,15 +206,15 @@ def main():
     rnn_params = {'input_size': 100*100*2, 'hidden_size': 256,
         'output_size': 4}
     # initialize RNN object
-    rnn = RNN(**rnn_params)
+    rnn = RNN2(**rnn_params)
     print(rnn)
     print()
 
     # define a loss function and optimizer
     criterion = nn.NLLLoss()
-    optimizer = optim.SGD(rnn.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(rnn.parameters(), lr=0.000001, momentum=0.9)
     # train model
-    train(rnn, X_train, y_train, criterion, optimizer)
+    train2(rnn, X_train, y_train, criterion, optimizer)
 
 if __name__ == '__main__':
     main()
