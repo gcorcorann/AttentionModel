6bf315db913937029e9ec2fed8fe8c739618b72a
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2017-12-02 16:45:21 -0500

    Added ability to read small mini batch of processed videos, and removed older RNN model that was "many-to-one".

--- src/rnn.py
+++ src/rnn.py
@@ -17,38 +17,6 @@ from torch.autograd import Variable
 import torch.optim as optim
 
 class RNN(nn.Module):
-    """ Vanilla RNN. """
-    def __init__(self, input_size, hidden_size, output_size):
-        """
-        Initialize RNN class.
-
-        @param  input_size:     input dimension size
-        @param  hidden_size:    number of hidden nodes
-        @param  output_size:    number of output units
-        """
-        super(RNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.h2o = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax()
-
-    def forward(self, input, hidden):
-        """
-        Forward pass through network.
-        """
-        combined = torch.cat((input, hidden), 1)
-        hidden = self.i2h(combined)
-        output = self.h2o(hidden)
-        output = self.softmax(output)
-        return output, hidden
-
-    def init_hidden(self, batch_size):
-        """
-        Initialize hidden units to zero.
-        """
-        return Variable(torch.zeros(batch_size, self.hidden_size))
-
-class RNN2(nn.Module):
     """ RNN based on image captioning. """
     def __init__(self, input_size, hidden_size, output_size):
         """
@@ -58,7 +26,7 @@ class RNN2(nn.Module):
         @param  hidden_size:    number of hidden nodes
         @param  output_size:    number of output units
         """
-        super(RNN2, self).__init__()
+        super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
@@ -89,20 +57,6 @@ class RNN2(nn.Module):
 import random
 import numpy as np
 
-def read_data(X_path, y_path):
-    """
-    Read and return data.
-
-    @param  X_path:     path to input data
-    @param  y_path:     path to input labels
-
-    @return X:          array of input data
-    @return y:          array of input labels
-    """
-    X = np.load(X_path)
-    y = np.load(y_path)
-    return X, y
-
 def random_training_example(X_train, y_train, batch_size):
     """
     Retrieve random training example.
@@ -130,38 +84,6 @@ def random_training_example(X_train, y_train, batch_size):
     return x, x_var, y, y_var
 
 def train(rnn, X_train, y_train, criterion, optimizer):
-    batch_size = 5
-    print('Training Model...')
-    for epoch in range(3):
-        print('Epoch:', epoch)
-        running_loss = 0.0
-        # go through all training examples
-        for i in range(X_train.shape[0]//batch_size):
-            print(i)
-            # get batch of random training examples
-            # nSequence x nBatch x nFeatures
-            x, x_var, y, y_var = random_training_example(X_train, y_train, 
-                    batch_size)
-            # zero the parameter gradients
-            optimizer.zero_grad()
-            # zero initial hidden state
-            hidden = rnn.init_hidden(batch_size)
-            # for each frame in sequence
-            for j in range(x_var.size()[0]):
-                # pass through RNN
-                output, hidden = rnn(x_var[j], hidden)
-            loss = criterion(output, y_var)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.data[0]
-        
-        # print statistics
-        print('Epoch Loss:', running_loss / X_train.shape[0] * batch_size)
-        running_loss = 0
-
-    print('Finished Training.')
-
-def train2(rnn, X_train, y_train, criterion, optimizer):
     batch_size = 5
     print('Training Model...')
     for epoch in range(5):
@@ -195,18 +117,33 @@ def train2(rnn, X_train, y_train, criterion, optimizer):
 
 def main():
     """ Main Function. """
+    from optical_flow import OpticalFlow
+    from video_dataset import VideoDataset
     print(__doc__)
-    # load small flow dataset
-    X_train, y_train = read_data('../data/X_flow_small.npy',
-            '../data/y_flow_small.npy')
-    print('X_train:', X_train.shape)
-    print('y_train:', y_train.shape)
+    # load dataset
+    labels_path = '../labels_gary.txt'
+    width = height = 100
+    # optical flow parameters
+    opt_params = {'pyr_scale': 0.5, 'levels': 3, 'winsize': 15, 'iterations': 3,
+        'poly_n': 5, 'poly_sigma': 1.2}
+    # create optical flow object
+    opt = OpticalFlow(**opt_params)
+    # create video data object
+    vids = VideoDataset(labels_path)
+    vids.set_video_params(width, height, processor=opt)
+    X, y = vids.read_data()
+    print('X:', len(X))
+    print('y:', len(y))
+    X_tr, y_tr, X_te, y_te = vids.partition_data(X, y, ratio=0.7)
+    X_batch, y_batch = vids.process_batch(X_tr, y_tr, batch_size=10) 
+    print(X_batch.shape, y_batch.shape)
+    print(X_batch.dtype, y_batch.dtype)
     print()
     # rnn parameters
-    rnn_params = {'input_size': 100*100*2, 'hidden_size': 256,
+    rnn_params = {'input_size': width*height*2, 'hidden_size': 256,
         'output_size': 4}
     # initialize RNN object
-    rnn = RNN2(**rnn_params)
+    rnn = RNN(**rnn_params)
     print(rnn)
     print()
 
@@ -214,7 +151,7 @@ def main():
     criterion = nn.NLLLoss()
     optimizer = optim.SGD(rnn.parameters(), lr=0.000001, momentum=0.9)
     # train model
-    train2(rnn, X_train, y_train, criterion, optimizer)
+    train(rnn, X_batch, y_batch, criterion, optimizer)
 
 if __name__ == '__main__':
     main()
