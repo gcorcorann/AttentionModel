273ef53bdd3434ecc0afbfb09aa79ff632f65042
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2018-02-26 19:05:18 -0500

    Added plotts.

--- src/display_annotations.py
+++ src/display_annotations.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """Read Attention Dataset Annotations.
 """
+import os
+import glob
 import numpy as np
+import matplotlib.pyplot as plt
 
 def read_labels(labels_path):
     """Read annotated labels and store the counts.
@@ -48,6 +51,14 @@ def compute_labels(path_list):
     return labels_med, labels_avg, videos
 
 def count_labels(labels_path):
+    """Compute number of labels in each attention level.
+
+    Args:
+        labels_path (string): path to attention labels
+
+    Returns:
+        ndarray: array consisting of counts for each label.
+    """
     counts = np.zeros(4)
     with open(labels_path, 'r') as f:
         for line in f:
@@ -56,53 +67,80 @@ def count_labels(labels_path):
 
     return counts
 
+def save_labels(labels_array, videos, name):
+    videos = np.array(videos)
+    annotations = np.vstack((videos, labels_array)).T
+    f = open(name, 'w')
+    for i in annotations:
+        s = str(i[0]) + " " + str(i[1][0]) + "\n"
+        f.write(s)
+
+    f.close()
+
+
 def main():
     """Main Function."""
-    labels_gary = '../data/labels_gary.txt'
-    labels_allison = '../data/labels_allison.txt'
-    labels_alexa = '../data/labels_alexa.txt'
-    labels_amanda = '../data/labels_amanda.txt'
-    paths = [labels_gary, labels_allison, labels_alexa, labels_amanda]
+    # labels datapth
+    labels_path = '/home/gary/datasets/accv/labels'
+
+    labels = glob.glob(os.path.join(labels_path, 'labels*.txt'))
     classes = ['Low Attention', 'Medium Attention',
             'High Attention', 'Very High Attention']
+    print(labels)
     print(classes)
     print()
 
-    print("Gary's Labels:   ", count_labels(labels_gary))
-    print("Allison's Labels:", count_labels(labels_allison))
-    print("Alexa's Labels:  ", count_labels(labels_alexa))
-    print("Amanda's Labels: ", count_labels(labels_amanda))
-    print()
+    # list to hold counts
+    counts = []
+    for path in labels:
+        print(path)
+        c = count_labels(path)
+        print(c)
+        counts.append(c)
 
-    labels_med, labels_avg, videos = compute_labels(paths)
+    # display distrubtion of labels
+    ind = np.arange(4)
+    width = 0.1
+    # hold bargraph info
+    rects = []
+    fig, ax = plt.subplots()
+    fig.set_size_inches(18.5, 10.5)
+    for i in range(len(labels)):
+        rects.append(ax.bar(ind, counts[i], width))
+        ind = ind + width
 
-#    counts = np.zeros(4)
-#    for l in labels_med:
-#        idx = int(l-1)
-#        counts[idx] += 1
+    ax.set_xlabel('Attention Levels')
+    ax.set_ylabel('Total Instances')
+    ax.set_title('Total Instances per Attention Level')
+    ax.set_xticks(ind-2*width)
+    ax.set_xticklabels(('Low\nAttention', 'Medium\nAttention', 
+        'High\nAttention', 'Very High\nAttention'))
+#    plt.show()
 #
-#    print("Median Labels:", counts)
-#
-    counts = np.zeros(4)
-    for l in labels_avg:
-        idx = int(l-1)
-        counts[idx] += 1
+    # save plots
+    labels_med, labels_avg, videos = compute_labels(labels)
+    median_path = os.path.join(labels_path, 'median_labels.txt')
+    average_path = os.path.join(labels_path, 'average_labels.txt')
+    save_labels(labels_med, videos, median_path)
+    save_labels(labels_avg, videos, average_path)
+    med_counts = count_labels(median_path)
+    avg_counts = count_labels(average_path)
+    
+    print(median_path)
+    print(med_counts)
+    print(average_path)
+    print(avg_counts)
 
-    print("Average Labels:", counts)
-    print(counts.sum())
+    # add distrubtions for median and average
+    rects.append(ax.bar(ind+width, med_counts, width))
+    rects.append(ax.bar(ind+2*width, avg_counts, width))
+    ax.legend((rects[0], rects[1], rects[2], rects[3], rects[4], rects[5],
+        rects[6]), ('Subject #1', 'Subject #2', 'Subject #3', 'Subject #4',
+            'Subject #5', 'Median', 'Average'))
 
-#    videos = np.array(videos)
-#    videos = np.vstack((videos, labels_med)).T
-#    print(videos)
-#
-#    file = open('annotations_med.txt', 'w')
-#
-#    for i in videos:
-#        s = str(i[0]) + " " + str(i[1][0]) + "\n"
-#        print(s)
-#        file.write(s)
-#
-#    file.close()
+    plt.show()
+    
+    fig.savefig('../plots/attention_distributions.png')
 
 if __name__ == '__main__':
     main()
